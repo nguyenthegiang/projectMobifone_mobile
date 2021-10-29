118f3506bafababcc95711dfaf5fa282d3af89f0
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-10-29 16:35:16 +0700

    Import Login Template: https://github.com/abymathewtkv/codiumLoginPage.git

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,120 @@
 import 'package:flutter/material.dart';
+import './UI/input_field.dart';
+import './home_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Mobifone App'),
+    return const MaterialApp(
+      //Material App
+      debugShowCheckedModeBanner: false,
+      title: "Login App",
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
-  final String title;
+  //StateFullWidget
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  HomeScreenState createState() => HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text(widget.title),
+      //Scaffold
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.lightBlueAccent,
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.bottomRight,
+              heightFactor: 0.5,
+              widthFactor: 0.5,
+              child: Material(
+                borderRadius: const BorderRadius.all(Radius.circular(200.0)),
+                color: const Color.fromRGBO(255, 255, 255, 0.4),
+                child: Container(
+                  width: 400,
+                  height: 400,
+                ),
+              ),
+            ),
+            Center(
+              child: Container(
+                width: 400,
+                height: 400,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Material(
+                        elevation: 10.0,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(50.0)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset(
+                            "assets/images/flutter-logo.png",
+                            width: 80,
+                            height: 80,
+                          ),
+                        )),
+                    Form(
+                      child: InputField(
+                          //Calling inputField  class
+
+                          const Icon(
+                            Icons.person,
+                            color: Colors.white,
+                          ),
+                          "Username"),
+                    ),
+                    Form(
+                      child: InputField(
+                          const Icon(
+                            Icons.lock,
+                            color: Colors.white,
+                          ),
+                          "Password"),
+                    ),
+                    Container(
+                      width: 150,
+                      child: RaisedButton(
+                        //Raised Button
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => MyHomePage()));
+                        },
+                        color: Colors.indigo,
+                        textColor: Colors.white,
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
