4f92bd77edc230d5d3f9f9eaeca42f4b159fb6eb
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-11-24 09:33:28 +0700

    Card1 - Menu

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -16,6 +16,7 @@ class HomeScreen extends StatelessWidget {
         //Logo
         title: Image.asset('assets/images/appBar_logo.png', fit: BoxFit.cover),
         backgroundColor: const Color.fromARGB(255, 26, 26, 38),
+        //3 nút trên AppBar
         actions: [
           IconButton(
             icon: const Icon(Icons.article_outlined),
@@ -34,7 +35,7 @@ class HomeScreen extends StatelessWidget {
             color: const Color.fromARGB(255, 71, 79, 100),
           ),
           IconButton(
-            icon: const Icon(Icons.account_box_rounded),
+            icon: const Icon(Icons.person),
             iconSize: 30.0,
             onPressed: () {
               //..
--- lib/Widget/card_1.dart
+++ lib/Widget/card_1.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './card_1_menu.dart';
+
 class Card1 extends StatelessWidget {
   const Card1({
     Key? key,
@@ -8,7 +10,79 @@ class Card1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Card(),
+      margin: const EdgeInsets.all(10),
+      child: Card(
+        child: Column(
+          children: [
+            //Radial Bar Chart
+            Container(
+              //Chart goes here!
+              height: 300,
+              child: Text('CHART!'),
+              color: Colors.greenAccent,
+            ),
+            //Vài Button xếp trong GridView
+            Container(
+              height: 400,
+              child: GridView(
+                padding: const EdgeInsets.all(25),
+                //Scrollable
+                physics: const ScrollPhysics(),
+                children: [
+                  //Extract Widget: card_1_menu.dart
+                  Card1Menu(
+                    menuIcon: Icon(
+                      Icons.bar_chart_rounded,
+                      color: Color.fromARGB(255, 254, 167, 0),
+                      size: 45,
+                    ),
+                    menuText: 'VLR trung bình tháng',
+                    backgroundColor: Color.fromARGB(255, 255, 244, 222),
+                    itemColor: Color.fromARGB(255, 254, 167, 0),
+                  ),
+                  Card1Menu(
+                    menuIcon: Icon(
+                      Icons.person_add_alt_1_rounded,
+                      color: Color.fromARGB(255, 54, 153, 255),
+                      size: 45,
+                    ),
+                    menuText: 'New Users',
+                    backgroundColor: Color.fromARGB(255, 224, 240, 255),
+                    itemColor: Color.fromARGB(255, 54, 153, 255),
+                  ),
+                  Card1Menu(
+                    menuIcon: Icon(
+                      Icons.collections_bookmark_rounded,
+                      color: Color.fromARGB(255, 246, 77, 98),
+                      size: 45,
+                    ),
+                    menuText: 'Item Orders',
+                    backgroundColor: Color.fromARGB(255, 255, 226, 230),
+                    itemColor: Color.fromARGB(255, 246, 77, 98),
+                  ),
+                  Card1Menu(
+                    menuIcon: Icon(
+                      Icons.bug_report_rounded,
+                      color: Color.fromARGB(255, 19, 197, 186),
+                      size: 45,
+                    ),
+                    menuText: 'Bug Reports',
+                    backgroundColor: Color.fromARGB(255, 201, 247, 245),
+                    itemColor: Color.fromARGB(255, 19, 197, 186),
+                  ),
+                ],
+                //Layout cho Grid
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200,
+                  childAspectRatio: 1,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
